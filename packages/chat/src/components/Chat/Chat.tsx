2b0aeee3828b032ae419881a80380ba7bb5cb7ba
Daniel Litvak <daniellitvak5@gmail.com>
2025-11-09 02:01:17 +0200

    Add Model Selector

--- packages/chat/src/components/Chat/Chat.tsx
+++ packages/chat/src/components/Chat/Chat.tsx
@@ -2,10 +2,12 @@ import './Chat.css';
 import { type FC } from "preact/compat";
 import { Messages } from '../Messages/Messages';
 import { Input } from '../Input/Input';
+import { ModelSelector } from '../ModelSelector/ModelSelector';
 
 export const Chat: FC = () => {
   return (
     <div className="chat-container">
+      <ModelSelector />
       <Messages />
       <Input />
       <div className="disclaimer" />
